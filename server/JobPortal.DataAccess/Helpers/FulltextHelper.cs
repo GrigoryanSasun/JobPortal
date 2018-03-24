8c5e5892a095ce33bf783507dcff61101972f3ae
Sasun Grigoryan <sasungrig@gmail.com>
2018-03-25 02:39:33 +0400

    Search by keyword implemented

---
+++ server/JobPortal.DataAccess/Helpers/FulltextHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.DataAccess.Helpers
+{
+    class FulltextHelper
+    {
+        public static string PreprocessForFulltextSearch(string keyword)
+        {
+            var words = keyword.Split(null).Select((word) => "\"" + word + "*\"");
+            return string.Join(" AND ", words);
+        }
+    }
+}
--- server/JobPortal.DataAccess/Repository/JobPostRepository.cs
+++ server/JobPortal.DataAccess/Repository/JobPostRepository.cs
@@ -1,8 +1,11 @@
-﻿using JobPortal.Context;
+﻿using JobPortal.Common.Model;
+using JobPortal.Context;
 using JobPortal.DataAccess.Core.Contract;
 using JobPortal.DataAccess.Core.Model;
+using JobPortal.DataAccess.Helpers;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +14,62 @@ namespace JobPortal.DataAccess.Repository
 {
     public class JobPostRepository : IJobPostRepository
     {
+        private string GetBaseJobPostQuery(string selectStatement, RepositoryJobPostSearchInputModel searchInputModel, out object[] sqlParams)
+        {
+            var sqlListParams = new List<object>();
+            StringBuilder sbQuery = new StringBuilder(string.Format(@"
+                {0}
+                FROM {1} jp
+                JOIN {2} et ON jp.EmploymentTypeId = et.Id
+                JOIN {3} jc ON jp.CategoryId = jc.Id
+                JOIN {4} jt ON jp.TitleId = jt.Id
+                JOIN {5} l ON jp.LocationId = l.Id
+                WHERE 1=1
+            ", 
+            selectStatement,
+            DbHelpers.JobPostsTableName, 
+            DbHelpers.EmploymentTypeTableName,
+            DbHelpers.JobCategoryTableName,
+            DbHelpers.JobTitleTableName,
+            DbHelpers.LocationTableName));
+            var keywordSearchType = searchInputModel.KeywordSearchType;
+            var keyword = searchInputModel.Keyword;
+            if ((keywordSearchType != null) && (!string.IsNullOrWhiteSpace(keyword)))
+            {
+                keyword = FulltextHelper.PreprocessForFulltextSearch(keyword);
+                string tablePrefixAndColumn = null;
+                switch (keywordSearchType)
+                {
+                    case KeywordSearchType.JobCategory:
+                        tablePrefixAndColumn = "jc.Name";
+                        break;
+                    case KeywordSearchType.Location:
+                        tablePrefixAndColumn = "l.Address";
+                        break;
+                    case KeywordSearchType.Title:
+                        tablePrefixAndColumn = "jt.Name";
+                        break;
+                }
+                if (!string.IsNullOrEmpty(tablePrefixAndColumn))
+                {
+                    sbQuery.AppendFormat(@"
+                        AND (CONTAINS({0}, @keyword))
+                    ", tablePrefixAndColumn);
+                    sqlListParams.Add(new SqlParameter("keyword", keyword));
+                }
+            }
+            sqlParams = sqlListParams.ToArray();
+            return sbQuery.ToString();
+        }
+
         public int GetJobPostCount(RepositoryJobPostSearchInputModel searchInputModel)
         {
             using (var jobPortalDbContext = new JobPortalDbContext())
             {
-                var jobPostTableName = DbHelpers.GetTableNameWithSchema(DbHelpers.JobPostsTableName);
-                var postCount = jobPortalDbContext.Database.SqlQuery<int>(string.Format("SELECT COUNT(*) FROM {0}", jobPostTableName)).First();
+                string selectStatement = "SELECT COUNT(*)";
+                object[] sqlParams = null;
+                string query = GetBaseJobPostQuery(selectStatement, searchInputModel, out sqlParams);
+                var postCount = jobPortalDbContext.Database.SqlQuery<int>(query, sqlParams).First();
                 return postCount;
             }
         }
@@ -25,15 +78,10 @@ namespace JobPortal.DataAccess.Repository
         {
             using (var jobPortalDbContext = new JobPortalDbContext())
             {
-                string query = @"
-                SELECT jp.Id AS Id, jp.IsBookmarked, jp.JobPostThumbnailUrl, jp.Views, jp.CreatedAt, et.Name AS EmploymentType, jc.Name AS JobCategory, jt.Name AS Title, l.Address AS Location
-                FROM JobPost jp
-                JOIN EmploymentType et ON jp.EmploymentTypeId = et.Id
-                JOIN JobCategory jc ON jp.CategoryId = jc.Id
-                JOIN JobTitle jt ON jp.TitleId = jt.Id
-                JOIN Location l ON jp.LocationId = l.Id;
-                ";
-                var result = jobPortalDbContext.Database.SqlQuery<RepositoryJobPostResult>(query).ToList();
+                string selectStatement = "SELECT jp.Id AS Id, jp.IsBookmarked, jp.JobPostThumbnailUrl, jp.Views, jp.CreatedAt, et.Name AS EmploymentType, jc.Name AS JobCategory, jt.Name AS Title, l.Address AS Location";
+                object[] sqlParams = null;
+                string query = GetBaseJobPostQuery(selectStatement, searchInputModel, out sqlParams);
+                var result = jobPortalDbContext.Database.SqlQuery<RepositoryJobPostResult>(query, sqlParams).ToList();
                 return result;
             }
         }
