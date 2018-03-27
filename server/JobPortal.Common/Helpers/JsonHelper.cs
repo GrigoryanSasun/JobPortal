f9027d683930141aae1b31c6cbd62ad6e8b6970d
Sasun Grigoryan <sasungrig@gmail.com>
2018-03-27 21:22:08 +0400

    Production theme loading implemented

---
+++ server/JobPortal.Common/Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Common.Helpers
+{
+    public static class JsonHelper
+    {
+        public static string ReadJsonFile(string path)
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                string json = streamReader.ReadToEnd();
+                return json;
+            }
+        }
+    }
+}
--- server/JobPortal.Web/Controllers/AngularSpaController.cs
+++ server/JobPortal.Web/Controllers/AngularSpaController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobPortal.Common.Helpers;
 using JobPortal.Web.ViewModel;
 using Newtonsoft.Json;
 
@@ -14,6 +15,13 @@ namespace JobPortal.Web.Controllers
         private string GetDevelopmentAssetUrl(string assetName)
         {
             return "/Content/dist/" + assetName;
+        }
+
+        private IEnumerable<DeserializedTheme> GetDeserializedThemes()
+        {
+            var themesJson = JsonHelper.ReadJsonFile(Server.MapPath("~/Themes.json"));
+            var themes = JsonConvert.DeserializeObject<DeserializedTheme[]>(themesJson);
+            return themes;
         }
 
         private AngularSpaResponseViewModel GetDevelopmentReponseViewModel()
@@ -32,40 +40,107 @@ namespace JobPortal.Web.Controllers
             var polyfillsScript = this.GetDevelopmentAssetUrl("polyfills.js");
             var vendorScript = this.GetDevelopmentAssetUrl("vendor.js");
             var mainScript = this.GetDevelopmentAssetUrl("main.js");
-            using (StreamReader r = new StreamReader(Server.MapPath("~/Themes.json")))
+            var themes = this.GetDeserializedThemes();
+            var themeDatas = new List<ThemeData>();
+            foreach (var theme in themes)
             {
-                string json = r.ReadToEnd();
-                var themes = JsonConvert.DeserializeObject<DeserializedTheme[]>(json);
-                var themeDatas = new List<ThemeData>();
-                foreach (var theme in themes)
+                var themeData = new ThemeData
                 {
-                    var themeData = new ThemeData
+                    ThemeId = theme.id,
+                    IsDefault = theme.is_default,
+                    StylesheetUrl = this.GetDevelopmentAssetUrl(theme.filename + ".css"),
+                    UIMainColor = theme.ui_main_color,
+                    UIDescription = theme.ui_description
+                };
+                themeDatas.Add(themeData);
+                if (themeData.IsDefault)
+                {
+                    defaultThemeData = new StylesheetData
                     {
-                        ThemeId = theme.id,
-                        IsDefault = theme.is_default,
-                        StylesheetUrl = this.GetDevelopmentAssetUrl(theme.filename + ".css"),
-                        UIMainColor = theme.ui_main_color,
-                        UIDescription = theme.ui_description
+                        IsTheme = true,
+                        StylesheetUrl = themeData.StylesheetUrl
                     };
-                    themeDatas.Add(themeData);
-                    if (themeData.IsDefault)
-                    {
-                        defaultThemeData = new StylesheetData
-                        {
-                            IsTheme = true,
-                            StylesheetUrl = themeData.StylesheetUrl
-                        };
-                    }
                 }
-                result.Themes = themeDatas;
             }
+            result.Themes = themeDatas;
             result.Stylesheets = new StylesheetData[3] { defaultThemeData, vendorCss, mainCss };
             result.Scripts = new string[4] { manifestScript, polyfillsScript, vendorScript, mainScript };
             return result;
         }
 
+        private AngularSpaResponseViewModel GetProductionResponseViewModel()
+        {
+            var result = new AngularSpaResponseViewModel();
+            var webpackAssetsPath = Server.MapPath("~/Content/dist/webpack-assets.json");
+            string webpackAssetsJson = JsonHelper.ReadJsonFile(webpackAssetsPath);
+            var assets = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(webpackAssetsJson);
+            var stylesheets = new List<StylesheetData>();
+            var scriptUrls = new List<string>();
+            dynamic manifestData;
+            if (assets.TryGetValue("manifest", out manifestData))
+            {
+                scriptUrls.Add(manifestData.js.ToString());
+            }
+            dynamic polyfillData;
+            if (assets.TryGetValue("polyfills", out polyfillData))
+            {
+                scriptUrls.Add(polyfillData.js.ToString());
+            }
+            dynamic vendorData;
+            if (assets.TryGetValue("vendor", out vendorData))
+            {
+                stylesheets.Add(new StylesheetData
+                {
+                    IsTheme = false,
+                    StylesheetUrl = vendorData.css.ToString()
+                });
+                scriptUrls.Add(vendorData.js.ToString());
+            }
+            dynamic mainData;
+            if (assets.TryGetValue("main", out mainData))
+            {
+                stylesheets.Add(new StylesheetData
+                {
+                    IsTheme = false,
+                    StylesheetUrl = mainData.css.ToString()
+                });
+                scriptUrls.Add(mainData.js.ToString());
+            }
+            var themes = this.GetDeserializedThemes();
+            var themeDatas = new List<ThemeData>();
+            foreach (var theme in themes)
+            {
+                dynamic themeAsset;
+                if (assets.TryGetValue(theme.filename, out themeAsset))
+                {
+                    var themeData = new ThemeData
+                    {
+                        ThemeId = theme.id,
+                        IsDefault = theme.is_default,
+                        StylesheetUrl = themeAsset.css.ToString(),
+                        UIMainColor = theme.ui_main_color,
+                        UIDescription = theme.ui_description
+                    };
+                    if (themeData.IsDefault)
+                    {
+                        stylesheets.Insert(0, new StylesheetData
+                        {
+                            IsTheme = true,
+                            StylesheetUrl = themeData.StylesheetUrl
+                        });
+                    }
+                    themeDatas.Add(themeData);
+                }
+            }
+            result.Stylesheets = stylesheets;
+            result.Scripts = scriptUrls;
+            result.Themes = themeDatas;
+            return result;
+        }
+
         public ActionResult Index()
         {
+            // If debugging is enabled, consider the app in development mode
             var isInDevelopment = HttpContext.IsDebuggingEnabled;
             AngularSpaResponseViewModel result = null;
             if (isInDevelopment)
@@ -74,31 +149,7 @@ namespace JobPortal.Web.Controllers
             }
             else
             {
-                var webpackAssetsPath = HttpContext.Server.MapPath("~/Content/dist/webpack-assets.json");
-                using (var streamReader = new StreamReader(webpackAssetsPath))
-                {
-                    string json = streamReader.ReadToEnd();
-                    dynamic assets = JsonConvert.DeserializeObject(json);
-                    var stylesheetUrls = new List<string>();
-                    var scriptUrls = new List<string>();
-                    var vendorCssUrl = assets.vendor.css;
-                    if (vendorCssUrl != null)
-                    {
-                        stylesheetUrls.Add(vendorCssUrl.ToString());
-                    }
-                    var mainCssUrl = assets.main.css;
-                    if (mainCssUrl != null)
-                    {
-                        stylesheetUrls.Add(mainCssUrl.ToString());
-                    }
-                    scriptUrls.Add(assets.manifest.js.ToString());
-                    scriptUrls.Add(assets.polyfills.js.ToString());
-                    scriptUrls.Add(assets.vendor.js.ToString());
-                    scriptUrls.Add(assets.main.js.ToString());
-                    //responseViewModel.Stylesheets = stylesheetUrls;
-                    result.Scripts = scriptUrls;
-                }
-
+                result = this.GetProductionResponseViewModel();
             }
             return View(result);
         }
